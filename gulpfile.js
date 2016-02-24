0f2f96383e6b269cccf668952e762d2a6a1d4d91
GaoQ1 <18912936976@qq.com>
2016-02-24 10:18:57 +0800

    add gulpfile min function

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,23 @@ var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var babel = require('gulp-babel');
 
+/*压缩和合并js和css的插件*/
+var gulpif = require('gulp-if');
+var eslint = require('gulp-eslint');
+var imagemin = require('gulp-imagemin');
+var minifyCss = require('gulp-minify-css');
+var minifyHtml = require('gulp-minify-html');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var useref = require('gulp-useref');
+
 /*browserify的配置文件*/
 var babelify = require('babelify');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+
 
 /*postcss的配置文件*/
 var cssnext = require('cssnext');
@@ -46,6 +57,13 @@ gulp.task('styles',function(){
         .pipe(gulp.dest('styles/dest'))
 });
 
+gulp.task('es6', function(){
+    return gulp.src('public/js/es6/*.js')
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(gulp.dest('public/js'));
+});
+
 //browserify配置
 gulp.task('watchify', function(){
     var bundler = watchify(browserify(opts));
@@ -68,3 +86,45 @@ gulp.task('watchify', function(){
 gulp.task('watch:styles',function(){
     gulp.watch('styles/*.scss',['styles']);
 });
+
+
+//代码的压缩和合并
+function lint(files,options){
+    return function(){
+        return gulp.src(files)
+            .pipe(eslint(options))
+            .pipe(eslint.format())
+    };
+};
+gulp.task('lint', lint('public/js/*.js'));
+
+gulp.task('html', ['styles'], function(){
+    var assets = useref.assets({searchPath: ['public']});
+
+    return gulp.src('public/appindex.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss({compatibility: '*'})))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulpif('public/appindex.html', minifyHtml({conditionals: true, loose: true})))
+        .pipe(gulp.dest('public/dist'));
+});
+
+//单个js和css文件的压缩
+/*gulp.task('componentjsmin',function(){
+    return gulp.src('public/component/!**!/!*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('newpublic/component'));
+});
+gulp.task('jsmin',function(){
+    return gulp.src(['public/js/!**!/!*.js','!public/js/browserify/!**!/!*.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('newpublic/js'));
+});
+gulp.task('cssmin',function(){
+    return gulp.src('public/!**!/!*.css')
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('newpublic'));
+});
+gulp.task('filemin',['cssmin','jsmin','componentjsmin']);*/
